60f74f438fa17bc8f0c171b0f0666602b9313294
dylanbram354 <dylanbram354@gmail.com>
2021-05-19 11:23:07 -0500

    Removed some test cases from main.py

--- main.py
+++ main.py
@@ -3,15 +3,7 @@ from game import Game
 
 if __name__ == '__main__':
     game = Game()
-    # p1 = game.player_one
-    # p1.name = 'P1'
-    # p2 = game.player_two
-    # p2.name = 'P2'
-    # p1.place_ships()
-    # # p2.place_ships()
-    # p1.display_opponent_board()
-    # # game.attack(p1, p2)
-
+    
     game.run_game()
-    # game.player_two.display_my_board()
+
 
